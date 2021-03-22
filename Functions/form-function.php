a7619ded55657da941c2b8dc23bbf22bc79545eb
stevendatle <70961170+stevendatle@users.noreply.github.com>
2021-03-22 19:00:38 -0400

    Update form-function.php

--- Functions/form-function.php
+++ Functions/form-function.php
@@ -5,7 +5,7 @@ function fillSelect($options, $select = "") {
   $html_dropdown = "";
   foreach ($options as $make) {
     $selected = $select == $make->make ? "selected" : "";
-    $html_dropdown .= "<option $selected value='$make->id'>$make->make</option>";
+    $html_dropdown .= "<option $selected value='$make->make_id'>$make->make</option>";
   }
   return $html_dropdown;
 }
